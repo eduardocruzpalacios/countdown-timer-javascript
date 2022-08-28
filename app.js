416e22ef8609d63942ea27abc6908c435738585a
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-08-28 17:04:35 +0200

    code more readable by putting functions definition after 1st call and the members they use before 1st call

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-// CONSTANTS & VARIABLES
+// CHECK INPUT IS SUPPORTED BY USER'S BROWSER AND CREATE CORRECT INPUT
+
+window.addEventListener('load', checkWhatInputBrowserSupports);
+
 const inputContainer = document.getElementById('setup');
 
-const decadesElement = document.getElementById("decades");
-const yearsElement = document.getElementById("years");
-const daysElement = document.getElementById("days");
-const hoursElement = document.getElementById("hours");
-const minutesElement = document.getElementById("minutes");
-const secondsElement = document.getElementById("seconds");
-const millisecondsElement = document.getElementById("milliseconds");
+function checkWhatInputBrowserSupports() {
+    if (checkInput('datetimeValue-local')) {
+        setLabel('Pick the date and time you wish:');
+        const dateTimeValueLocal = document.createElement('input');
+        dateTimeValueLocal.setAttribute('type', 'datetimeValue-local');
+        dateTimeValueLocal.setAttribute('id', 'datetimeValue');
+        inputContainer.appendChild(dateTimeValueLocal);
+        setButton();
+    } else if (checkInput('date') && checkInput('time')) {
+        setLabel('Date:');
+        const date = document.createElement('input');
+        date.setAttribute('type', 'date');
+        date.setAttribute('id', 'date');
+        inputContainer.appendChild(date);
+        setLabel('Time:');
+        const time = document.createElement('input');
+        time.setAttribute('type', 'time');
+        time.setAttribute('id', 'time');
+        time.setAttribute('value', '00:00');
+        inputContainer.appendChild(time);
+        setButton();
+    } else {
+        alert('Sorry, your browser does not support this app');
+    }
+}
 
-let datetime;
-const error = document.getElementById('errormsg');
-const count = document.getElementById('count');
-
-let difference;
-
-let endDate;
-let currentDate;
-let totalSeconds;
-
-let interval;
-let interval2;
-const refreshElement = document.getElementById('refresh');
-let refreshTime = 1;
-
-const progressBarElement = document.getElementById('progressBar');
-const percentageElement = document.getElementById('percentage');
-const progressElement = document.getElementById('progress');
-
-let secondsCurrent = 0;
-
-let running = false;
-
-// CHECK INPUT IS SUPPORTED BY USER'S BROWSER AND CREATE CORRECT INPUT
 function checkInput(type) {
     const input = document.createElement('input');
     input.setAttribute('type', type);
     return input.type === type;
+}
+
+function setLabel(text) {
+    const label = document.createElement('label');
+    label.innerHTML = text;
+    inputContainer.appendChild(label);
 }
 
 function setButton() {
@@ -47,112 +50,71 @@ function setButton() {
     inputContainer.appendChild(button);
 }
 
-function setLabel(text) {
-    const label = document.createElement('label');
-    label.innerHTML = text;
-    inputContainer.appendChild(label);
-}
-
-if (checkInput('datetime-local')) {
-    // LABEL
-    setLabel('Pick the date and time you wish:');
-    // INPUT DATETIME-LOCAL
-    const dateTimeLocal = document.createElement('input');
-    dateTimeLocal.setAttribute('type', 'datetime-local');
-    dateTimeLocal.setAttribute('id', 'datetime');
-    inputContainer.appendChild(dateTimeLocal);
-    // BUTTON
-    setButton();
-} else if (checkInput('date') && checkInput('time')) {
-    // LABEL
-    setLabel('Date:');
-    // INPUT DATE
-    const date = document.createElement('input');
-    date.setAttribute('type', 'date');
-    date.setAttribute('id', 'date');
-    inputContainer.appendChild(date);
-    // LABEL
-    setLabel('Time:');
-    // INPUT TIME
-    const time = document.createElement('input');
-    time.setAttribute('type', 'time');
-    time.setAttribute('id', 'time');
-    time.setAttribute('value', '00:00');
-    inputContainer.appendChild(time);
-    // BUTTON
-    setButton();
-} else {
-    alert('Sorry, your browser does not support this app');
-}
-
 // IT TRIGGERS WHEN START BUTTON IS CLICKED
-function start() {
 
-    if (checkInput('datetime-local')) {
-        datetime = document.getElementById('datetime').value;
+let datetimeValue;
+let difference;
+const error = document.getElementById('errormsg');
+let isRunning = false;
+const count = document.getElementById('count');
+const progressBarElement = document.getElementById('progressBar');
+let interval;
+let secondsCurrent = 0;
+let interval2;
+
+function start() {
+    if (checkInput('datetimeValue-local')) {
+        datetimeValue = document.getElementById('datetimeValue').value;
     } else if (checkInput('date') && checkInput('time')) {
         let date = document.getElementById('date').value;
-        // console.log(date);
-
         let time = document.getElementById('time').value;
-        // console.log(time);
-
-        datetime = date + 'T' + time;
+        datetimeValue = date + 'T' + time;
     }
 
-    // console.log(datetime);
-
-    const end = new Date(datetime);
+    const end = new Date(datetimeValue);
     const now = new Date();
-    difference = (end - now);
-    // console.log(difference);
+    difference = end - now;
 
-    // VALIDATE SELECTED DATE BY USER (16 IS THE DATETIME FORMAT LENGTH)
-    if (datetime == '' || datetime == null || datetime.length < 16) {
-        error.innerHTML = "set up a date!";
+    if (
+        datetimeValue == '' ||
+        datetimeValue == null ||
+        datetimeValue.length < 16
+    ) {
+        error.innerHTML = 'set up a date!';
     } else if (difference <= 0) {
-        if (!running) {
-            error.innerHTML = "the end date must be later than now!";
+        if (!isRunning) {
+            error.innerHTML = 'the end date must be later than now!';
         } else {
-            // console.log('running: ' + running);
-            running = false;
+            isRunning = false;
         }
     } else {
-        running = true;
-        error.innerHTML = "";
-        error.style.display = "none";
+        isRunning = true;
+        error.innerHTML = '';
+        error.style.display = 'none';
         count.style.display = 'flex';
         progressBarElement.style.display = 'block';
-        // console.log(datetime);
-        // SAVE INTO A VARIABLE TO BE ABLE TO STOP WITH CLEARINTERVAL LATER
         interval = setInterval(countdown, refreshTime);
-
-        // PROGRESS BAR
-        // console.log(secondsTotal);
         secondsCurrent = 0;
         clearInterval(interval2);
         interval2 = setInterval(progressBar, 100);
     }
 }
 
-// REFRESH TIME CONFIG
+let endDate;
+let currentDate;
+let totalSeconds;
+const millisecondsElement = document.getElementById('milliseconds');
+const secondsElement = document.getElementById('seconds');
+const minutesElement = document.getElementById('minutes');
+const hoursElement = document.getElementById('hours');
+const daysElement = document.getElementById('days');
+const yearsElement = document.getElementById('years');
+const decadesElement = document.getElementById('decades');
 
-refreshElement.addEventListener('input', function () {
-    refreshTime = refreshElement.value;
-    // console.log(refreshTime);
-    clearInterval(interval);
-    start();
-}, false);
-
-// COUNTDOWN
 function countdown() {
-    endDate = new Date(datetime);
-    // console.log(endDate);
-
+    endDate = new Date(datetimeValue);
     currentDate = new Date();
-    // console.log(currentDate);
-
-    totalSeconds = (endDate - currentDate);
+    totalSeconds = endDate - currentDate;
 
     if (totalSeconds >= 0) {
         const milliseconds = Math.floor(totalSeconds) % 1000;
@@ -182,62 +144,78 @@ function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
-// PROGRESS BAR
-function progressBar() {
-
-    // EACH X MILLISECONDS, let += X MILLISECONDS
-    secondsCurrent += 100;
-    // console.log("secondsCurrent: " + secondsCurrent);
-
-    // console.log(difference);
-
-    let percentage = secondsCurrent / difference * 100;
-    // console.log(percentage);
-
-    if (percentage <= 100 && secondsCurrent <= difference) {
-
-        progressElement.style.width = percentage + "%";
-
-        let decimals;
-
-        if (difference <= 10000) { // < 10 SECONDS
-            decimals = percentage.toFixed(0);
-        } else if (difference > 10000 && difference <= 60000) { // < 1 MINUTE
-            decimals = percentage.toFixed(1);
-        } else if (difference > 60000 && difference <= 600000) { // < 10 MINUTES
-            decimals = percentage.toFixed(2);
-        } else if (difference > 600000 && difference <= 3600000) { // < 1 HOUR
-            decimals = percentage.toFixed(3);
-        } else if (difference > 3600000 && difference <= 86400000) { // > 1 HOUR
-            decimals = percentage.toFixed(4);
-        } else if (difference > 86400000 && difference <= 2592000000) { // > 1 DAY
-            decimals = percentage.toFixed(5);
-        } else if (difference > 2592000000 && difference <= 31104000000) { // > 30 DAYS
-            decimals = percentage.toFixed(6);
-        } else if (difference > 31104000000 && difference <= 311040000000) { // > 1 YEAR
-            decimals = percentage.toFixed(7);
-        } else { // > 10 YEARS
-            decimals = percentage.toFixed(8);
-        }
-
-        percentageElement.innerHTML = decimals + " %";
-    }
-}
-
-// COUNT FINISHED
 function countFinished() {
     clearInterval(interval);
     clearInterval(interval2);
-    error.style.display = "block";
-    error.innerHTML = "";
-    if (!running) {
-        error.innerHTML = "invalid date selected while running";
-        progressBarElement.style.display = "none";
-        count.style.display = "none";
+    error.style.display = 'block';
+    error.innerHTML = '';
+    if (!isRunning) {
+        error.innerHTML = 'invalid date selected while isRunning';
+        progressBarElement.style.display = 'none';
+        count.style.display = 'none';
     } else {
-        error.innerHTML = "the count has ended!";
-        progressBarElement.style.display = "block";
-        count.style.display = "flex";
-        running = false;
+        error.innerHTML = 'the count has ended!';
+        progressBarElement.style.display = 'block';
+        count.style.display = 'flex';
+        isRunning = false;
     }
 }
+
+const percentageElement = document.getElementById('percentage');
+const progressElement = document.getElementById('progress');
+
+function progressBar() {
+    secondsCurrent += 100;
+    let percentage = (secondsCurrent / difference) * 100;
+
+    if (percentage <= 100 && secondsCurrent <= difference) {
+        progressElement.style.width = percentage + '%';
+        let decimals;
+
+        if (difference <= 10000) {
+            // < 10 SECONDS
+            decimals = percentage.toFixed(0);
+        } else if (difference > 10000 && difference <= 60000) {
+            // < 1 MINUTE
+            decimals = percentage.toFixed(1);
+        } else if (difference > 60000 && difference <= 600000) {
+            // < 10 MINUTES
+            decimals = percentage.toFixed(2);
+        } else if (difference > 600000 && difference <= 3600000) {
+            // < 1 HOUR
+            decimals = percentage.toFixed(3);
+        } else if (difference > 3600000 && difference <= 86400000) {
+            // > 1 HOUR
+            decimals = percentage.toFixed(4);
+        } else if (difference > 86400000 && difference <= 2592000000) {
+            // > 1 DAY
+            decimals = percentage.toFixed(5);
+        } else if (difference > 2592000000 && difference <= 31104000000) {
+            // > 30 DAYS
+            decimals = percentage.toFixed(6);
+        } else if (difference > 31104000000 && difference <= 311040000000) {
+            // > 1 YEAR
+            decimals = percentage.toFixed(7);
+        } else {
+            // > 10 YEARS
+            decimals = percentage.toFixed(8);
+        }
+
+        percentageElement.innerHTML = decimals + ' %';
+    }
+}
+
+// REFRESH PACE
+
+const refreshElement = document.getElementById('refresh');
+let refreshTime = 1;
+
+refreshElement.addEventListener(
+    'input',
+    function () {
+        refreshTime = refreshElement.value;
+        clearInterval(interval);
+        start();
+    },
+    false
+);
